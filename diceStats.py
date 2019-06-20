984ba89f312a39e7f52053195c47e4c22008b2de
Elijah Barrett <elijahbarrett22@gmail.com>
2019-06-20 08:07:40 -0500

    create getMode, get Median, getMean

--- diceStats.py
+++ diceStats.py
@@ -1,6 +1,18 @@
 import random
 
-#def roll():
-#    return random(1,6)
+def roll():
+    return int((random.random()*6)+1)
 
-print(int((random.random()*6)+1))
+def getMean(data):
+    return sum(data)
+
+def getMode(data):
+    return
+
+def getMedian(data, N):
+    data.sort()
+    if N%2 = 0:
+        median = (data[(N/2)-1] + data[N/2])/2
+    else:
+        median = data[int(N/2)]
+    return median
